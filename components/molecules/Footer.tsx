aeeb0098d9808a6dea79a3cc7e0fb4f292347892
rizkianakbar <rizkianakbar4@gmail.com>
2022-07-07 08:03:37 +0700

    fix: footer gap

--- components/molecules/Footer.tsx
+++ components/molecules/Footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
   return (
     <footer className='flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8'>
       <hr className='w-full border-1 border-gray-200 dark:border-gray-800 mb-8' />
-      <div className='w-full max-w-2xl grid grid-cols-3 pb-16 sm:grid-cols-4 gap-4'>
+      <div className='w-full max-w-2xl grid grid-cols-3 pb-16 sm:grid-cols-4 gap-0 md:gap-4'>
         <div className='sm:flex flex-col hidden sm:visible'>
           <SignatureIcon />
         </div>
