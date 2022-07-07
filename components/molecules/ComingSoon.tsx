5500ae528f2c4f154db27fd204159fb7d7711895
rizkianakbar <rizkianakbar4@gmail.com>
2022-07-07 07:38:43 +0700

    feat: coming soon component

---
+++ components/molecules/ComingSoon.tsx
@@ -0,0 +1,9 @@
+export default function CommingSoon({ name }: { name: string }) {
+  return (
+    <div className='flex justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto mb-[50px]'>
+      <h1 className='font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white'>
+        {name} Comming soon...
+      </h1>
+    </div>
+  );
+}
