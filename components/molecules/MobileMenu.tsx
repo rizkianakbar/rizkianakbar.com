d716a2e11bd0a2c91d5ef5079c7d0ba12b52ad70
rizkianakbar <rizkianakbar4@gmail.com>
2022-07-07 07:40:47 +0700

    feat: mobile menu component

---
+++ components/molecules/MobileMenu.tsx
@@ -0,0 +1,139 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import useDelayedRender from 'use-delayed-render';
+
+import styles from 'styles/mobile-menu.module.css';
+
+import clsxm from '@/lib/clsxm';
+
+export default function MobileMenu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
+    isMenuOpen,
+    {
+      enterDelay: 20,
+      exitDelay: 300,
+    }
+  );
+
+  function toggleMenu() {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+      document.body.style.overflow = '';
+    } else {
+      setIsMenuOpen(true);
+      document.body.style.overflow = 'hidden';
+    }
+  }
+
+  useEffect(() => {
+    return function cleanup() {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
+  return (
+    <>
+      <button
+        className={clsxm(styles.burger, 'visible md:hidden')}
+        aria-label='Toggle menu'
+        type='button'
+        onClick={toggleMenu}
+      >
+        <MenuIcon data-hide={isMenuOpen} />
+        <CrossIcon data-hide={!isMenuOpen} />
+      </button>
+      {isMenuMounted && (
+        <ul
+          className={clsxm(
+            styles.menu,
+            'flex flex-col bg-gray-50/50 dark:bg-gray-900/50 backdrop-blur',
+            isMenuRendered && styles.menuRendered
+          )}
+        >
+          <li
+            className=' dark:border-gray-700 text-gray-900 dark:text-gray-100 text-xl font-bold'
+            style={{ transitionDelay: '150ms' }}
+          >
+            <Link href='/'>
+              <a className='flex w-auto'>Home</a>
+            </Link>
+          </li>
+          <li
+            className=' dark:border-gray-700 text-gray-900 dark:text-gray-100 text-xl font-bold'
+            style={{ transitionDelay: '175ms' }}
+          >
+            <Link href='/about'>
+              <a className='flex w-auto'>About</a>
+            </Link>
+          </li>
+          <li
+            className=' dark:border-gray-700 text-gray-900 dark:text-gray-100 text-xl font-bold'
+            style={{ transitionDelay: '200ms' }}
+          >
+            <Link href='/project'>
+              <a className='flex w-auto'>Project</a>
+            </Link>
+          </li>
+          <li
+            className=' dark:border-gray-700 text-gray-900 dark:text-gray-100 text-xl font-bold'
+            style={{ transitionDelay: '250ms' }}
+          >
+            <Link href='/blog'>
+              <a className='flex w-auto'>Blog</a>
+            </Link>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+function MenuIcon(props: JSX.IntrinsicElements['svg']) {
+  return (
+    <svg
+      className='h-5 w-5 absolute text-gray-900 dark:text-gray-100'
+      width='20'
+      height='20'
+      viewBox='0 0 20 20'
+      fill='none'
+      {...props}
+    >
+      <path
+        d='M2.5 7.5H17.5'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M2.5 12.5H17.5'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}
+
+function CrossIcon(props: JSX.IntrinsicElements['svg']) {
+  return (
+    <svg
+      className='h-5 w-5 absolute text-gray-900 dark:text-gray-100'
+      viewBox='0 0 24 24'
+      width='24'
+      height='24'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      fill='none'
+      shapeRendering='geometricPrecision'
+      {...props}
+    >
+      <path d='M18 6L6 18' />
+      <path d='M6 6l12 12' />
+    </svg>
+  );
+}
