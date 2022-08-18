972a2597624ad66af1d2f4d13e434a90e78f4fed
rizkianakbar <rizkianakbar4@gmail.com>
2022-08-18 11:44:42 +0700

    fix: change url for umami analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
             async
             defer
             data-website-id='e641a06f-ea57-4937-b162-2a56944ddf34'
-            src='https://umami-snowy-iota.vercel.app/umami.js'
+            src='https://umami-rizkianakbar.vercel.app/umami.js'
           ></script>
         </Head>
         <body>
